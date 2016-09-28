3805cc1519b602602c53c9a8d0cddc6030ca33bf
dymyw <dymayongwei@163.com>
2016-09-28 14:47:50 +0800

    Catalog

---
+++ Www/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Application entrance
+ *
+ * @author Dymyw <dymayongwei@163.com>
+ * @since 2014-09-11
+ * @version 2016-09-28
+ */
+
+include_once 'init.inc.php';
+
+echo ROOT_DIR;
---
+++ Www/init.inc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Application initialization supporting file
+ *
+ * @author Dymyw <dymayongwei@163.com>
+ * @since 2014-09-11
+ * @version 2016-09-28
+ */
+
+/**
+ * Filesystem constants
+ */
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_DIR',  dirname(__DIR__) . DS);
+define('CORE_DIR',  ROOT_DIR . 'Core' . DS);
+define('APP_DIR',   ROOT_DIR . 'App' . DS);
+define('WWW_DIR',   ROOT_DIR . 'Www' . DS);
+
+define('MC_VERSION', '2.0');
