60dd151c200ebf4dcd2efebd7fc47479c25732da
dymyw <dymayongwei@163.com>
2016-09-29 11:10:14 +0800

    Core\Loader\AutoLoader

--- Www/index.php
+++ Www/index.php
@@ -4,9 +4,9 @@
  *
  * @author Dymyw <dymayongwei@163.com>
  * @since 2014-09-11
- * @version 2016-09-28
+ * @version 2016-09-29
  */
 
 include_once 'init.inc.php';
 
-echo ROOT_DIR;
+echo 'Core\Loader\AutoLoader';
--- Www/init.inc.php
+++ Www/init.inc.php
@@ -4,8 +4,10 @@
  *
  * @author Dymyw <dymayongwei@163.com>
  * @since 2014-09-11
- * @version 2016-09-28
+ * @version 2016-09-29
  */
+
+use Core\Loader\AutoLoader;
 
 /**
  * Filesystem constants
@@ -15,5 +17,15 @@ define('ROOT_DIR',  dirname(__DIR__) . DS);
 define('CORE_DIR',  ROOT_DIR . 'Core' . DS);
 define('APP_DIR',   ROOT_DIR . 'App' . DS);
 define('WWW_DIR',   ROOT_DIR . 'Www' . DS);
+
+/**
+ * Register autoload
+ */
+include_once CORE_DIR . 'Loader' . DS . 'AutoLoader.php';
+AutoLoader::setNamespaces([
+    'Core' => CORE_DIR,
+    'App' => APP_DIR . 'Class' . DS,
+]);
+AutoLoader::register();
 
 define('MC_VERSION', '2.0');
