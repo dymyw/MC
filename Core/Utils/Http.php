76a64dcc3aacdf29c4eee197d375faac793727ba
dymyw <dymayongwei@163.com>
2016-11-08 15:45:16 +0800

    Core\Utils\Http

---
+++ Core/Utils/Http.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Http utils
+ *
+ * @package Core_Utils
+ * @author Dymyw <dymayongwei@163.com>
+ * @since 2015-01-19
+ * @version 2016-11-08
+ */
+
+namespace Core\Utils;
+
+class Http
+{
+    /**
+     * @param string $type
+     * @param string|null $charset
+     * @example
+     *  Http::mimeType('html', 'utf-8');
+     *  Http::mimeType('exe');
+     */
+    public static function mimeType($type, $charset = null)
+    {
+        $types = [
+            'html'  => 'text/html',
+            'xml'   => 'text/xml',
+            'css'   => 'text/css',
+            'txt'   => 'text/plain',
+            'json'  => 'application/json',
+            'jsonp' => 'application/javascript',
+            'js'    => 'application/javascript',
+            'rss'   => 'application/rss+xml',
+            'atom'  => 'application/atom+xml',
+            'xhtml' => 'application/xhtml+xml',
+
+            'gif'   => 'image/gif',
+            'jpg'   => 'image/jpeg',
+            'png'   => 'image/png',
+            'ico'   => 'image/x-icon',
+            'bmp'   => 'image/x-ms-bmp',
+            'flv'   => 'video/x-flv',
+            'doc'   => 'application/msword',
+            'xls'   => 'application/vnd.ms-excel',
+            'csv'   => 'application/vnd.ms-excel',
+            'pdf'   => 'application/pdf',
+            'zip'   => 'application/zip',
+            'rar'   => 'application/x-rar-compressed',
+            'swf'   => 'application/x-shockwave-flash',
+            '*'     => 'application/octet-stream',
+        ];
+
+        if (!isset($types[$type])) {
+            $type = '*';
+        }
+
+        $contentType = 'Content-Type: ' . $types[$type];
+        if ($charset) {
+            $contentType .= '; charset=' . $charset;
+        }
+        header($contentType);
+    }
+
+    /**
+     * Content-Disposition header
+     *
+     * @param string $filename
+     */
+    public static function disposition($filename)
+    {
+        header('Content-Disposition: attachment; filename=' . $filename);
+    }
+}
--- Www/init.inc.php
+++ Www/init.inc.php
@@ -4,24 +4,30 @@
  *
  * @author Dymyw <dymayongwei@163.com>
  * @since 2014-09-11
- * @version 2016-11-02
+ * @version 2016-11-08
  */
 
 use Core\Loader\AutoLoader;
 use Core\ServiceLocator\ServiceLocator;
+use Core\Utils\Http;
+
+/**
+ * MC Version
+ */
+define('MC_VERSION', '2.0');
 
 /**
  * Filesystem constants
  */
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT_DIR',  dirname(__DIR__) . DS);
-define('CORE_DIR',  ROOT_DIR . 'Core' . DS);
+define('ROOT_DIR',      dirname(__DIR__) . DS);
+define('CORE_DIR',      ROOT_DIR . 'Core' . DS);
 
 define('APP_DIR',       ROOT_DIR . 'App' . DS);
 define('CONFIG_DIR',    APP_DIR . 'Config' . DS);
 define('TPL_DIR',       APP_DIR . 'Template' . DS);
 
-define('WWW_DIR',   ROOT_DIR . 'Www' . DS);
+define('WWW_DIR',       ROOT_DIR . 'Www' . DS);
 
 /**
  * The base path of URLs
@@ -40,8 +46,8 @@ if (file_exists(CONFIG_DIR . 'Config.private.php')) {
  */
 include_once CORE_DIR . 'Loader' . DS . 'AutoLoader.php';
 AutoLoader::setNamespaces([
-    'Core' => CORE_DIR,
-    'App' => APP_DIR . 'Class' . DS,
+    'Core'  => CORE_DIR,
+    'App'   => APP_DIR . 'Class' . DS,
 ]);
 AutoLoader::register();
 
@@ -57,4 +63,5 @@ AutoLoader::register();
 /* @var $locator ServiceLocator */
 $GLOBALS['locator'] = new ServiceLocator(include CONFIG_DIR . 'Service.php');
 
-define('MC_VERSION', '2.0');
+// render page via utf8 charset
+Http::mimeType('html', 'utf-8');
